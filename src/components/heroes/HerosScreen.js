062e307bd5ee340907ffcef79ec88c990dcc7ad7
jaime sanchez galvis <jaimesanchezgalvis@gmail.com>
2021-08-18 19:56:22 -0500

    HeroApp end

--- src/components/heroes/HerosScreen.js
+++ src/components/heroes/HerosScreen.js
@@ -41,7 +41,7 @@ export const HerosScreen = ({ history }) => {
             <div className="col-8">
                 <h3>{superhero}</h3>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
+                    <li className="list-group-item"><b>Alter Ego: </b>{alter_ego}</li>
                     <li className="list-group-item"><b>Publisher: </b>{publisher}</li>
                     <li className="list-group-item"><b>First appearance: </b>{first_appearance}</li>
                 </ul>
